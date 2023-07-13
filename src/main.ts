4263fb3ba026d2ef4b7c02fc8880a383a1b1e67f
Jonathan Nagayoshi <jonathan@nagayoshi.com.br>
2023-07-12 22:01:41 -0500

    feat(setup): initial setup of the action

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const role_arn: string = core.getInput('role_arn')
+    const image: string = core.getInput("image")
+    core.debug(`Using ${role_arn} to authenticate to AWS`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    core.debug(`Using ${image} as the container image for running the task`)
+   
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
